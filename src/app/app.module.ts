6f3de2dc286b6f5b5536be5761eab9d4098008fd
geovanneaoliveira <geovanne@vortexnet.com.br>
2022-10-07 21:37:17 -0300

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomButtonComponent } from './custom-button/custom-button.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomButtonComponent,
+    LoginComponentComponent,
   ],
   imports: [
     BrowserModule,
