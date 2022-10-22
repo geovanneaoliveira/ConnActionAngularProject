7d495d3a4670ebc2325607eb1d2bea670a3caff1
Larissa Ferreira de Souza Legal <llegal@furb.br>
2022-10-21 21:01:21 -0300

    atual

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AtualizarCadastroComponent } from './atualizar-cadastro/atualizar-cadastro.component';
 import { CadastroComponentComponent } from './cadastro-component/cadastro-component.component';
-import { FazerContribuicaoComponent } from './fazer-contribuicao/fazer-contribuicao.component';
+import { CriarContribuicaoComponent } from './criar-contribuicao/criar-contribuicao.component';
 import { CriarEventoComponent } from './criar-evento/criar-evento.component';
 import { QuemSomosComponent } from './quem-somos/quem-somos.component';
-import { TesteComponent } from './teste/teste.component';
 import { VagasOrganizacaoComponent } from './vagas-organizacao/vagas-organizacao.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
@@ -76,14 +75,6 @@ const routes: Routes = [
     canActivate: [AuthGuard]
   },
   {
-    path:'teste',
-    component:TesteComponent
-  },
-  {
-    component: TesteComponent,
-    canActivate: [AuthGuard]
-  },
-  {
     path: 'atualizarCadastro',
     component: AtualizarCadastroComponent,
     canActivate: [AuthGuard]
@@ -104,7 +95,7 @@ const routes: Routes = [
   },
   {
     path: 'contribuicao',
-    component: FazerContribuicaoComponent
+    component: CriarContribuicaoComponent
   },
   {
     path: '**',
