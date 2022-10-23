57ec782db19c16a1dfc604ae2e4373e872adc1d6
Geovanne Almeida <geovanne@vortexnet.com.br>
2022-10-23 18:04:18 -0300

    a

--- src/app/criar-contribuicao/criar-contribuicao.component.ts
+++ src/app/criar-contribuicao/criar-contribuicao.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { ContribuicaoService } from '../services/contribuicao.service';
 import { OrganizacaoService } from '../services/organizacao.service';
+import { ContribuicaoForm, Organizacao } from '../types/types';
 
 @Component({
   selector: 'app-criar-contribuicao',
@@ -12,25 +13,46 @@ export class CriarContribuicaoComponent implements OnInit {
 
   contribuicaoForm;
   submitted=false;
+  todasOrgs:Organizacao[] = [];
+  idOrg!: number;
+  contribuicao: any;
 
   constructor(private formBuilder:FormBuilder, private contribuicaoService:ContribuicaoService, private organizacaoService:OrganizacaoService) {
     this.contribuicaoForm = formBuilder.group({
-      valor:['', Validators.required],
+      valor:['',Validators.required],
       data:[new Date()],
       idPessoa:[contribuicaoService.auth.userValue.id],
-      idOrganizacao:[organizacaoService]
-    })
+      idOrganizacao:[this.idOrg]
+    });
   }
 
   ngOnInit(): void {
+    this.organizacaoService.getAll().subscribe(organizacoes => {
+      this.todasOrgs = organizacoes;
+    });
   }
 
   get f() {
     return this.contribuicaoForm.controls;
   }
 
-  criarContribuicao = () => {
-
+  setIdOrg = (idOrg:number) => {
+    this.idOrg = idOrg;
   }
 
+  criarContribuicao = () => {
+    this.submitted=true;
+    this.contribuicaoForm.value.idOrganizacao = this.idOrg;
+    this.contribuicaoService.criarContribuicao(this.contribuicaoForm.value as ContribuicaoForm).subscribe({
+      next: (response) => {
+        console.log(response);
+        window.location.reload();
+      },
+      error: (error) => console.log(error),
+    });
+  }
+
+  conhecerOrg = () => {
+
+  }
 }
